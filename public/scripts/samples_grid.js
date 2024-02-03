833ea93eb8c7c47be5ea1317b57b9184d5a16e88
NoahTheLegend <20jeka03@gmail.com>
2024-02-03 19:01:29 +0200

    refactor scripts

--- public/scripts/samples_grid.js
+++ public/scripts/samples_grid.js
@@ -1,17 +1,10 @@
-import { connect_style } from "./style_connector.js";
+import { connectStyle } from "./utils/style_connector.js";
 
 export const get_samples_grid = () => {
-    connect_style("samples_grid.css");
+    connectStyle("samples_grid.css");
 
     const content = document.createElement("div");
     content.id = "samples_grid";
-
-    if (typeof process == "undefined" || !process.version) {
-        content.textContent = "Requires Node.js";
-        return content;
-    }
-    
-    console.log("Node.js version: "+process.version);
 
     return content;
 }
